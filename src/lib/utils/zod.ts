97c7bd1e850cf700e0eec45bcbf9afbcc136c3e3
NanezX <victor.nanej@gmail.com>
2025-01-29 07:30:29 -0400

    feat: rename UserAuthSchema to UserLoginSchema and add UserRegisterSchema for registration validation

--- src/lib/utils/zod.ts
+++ src/lib/utils/zod.ts
@@ -1,6 +1,10 @@
 import { z } from "zod"
 
-export const UserAuthSchema = z.object({
+export const UserLoginSchema = z.object({
     email: z.string().min(1, "Email is required").email("Invalid email address"),
     password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export const UserRegisterSchema = UserLoginSchema.extend({
+    fullName: z.string().min(1, "Name is required"),
 });
