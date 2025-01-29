7796fdfc4fc94a7e0adf4766b2c1637c070cc11c
NanezX <victor.nanej@gmail.com>
2025-01-29 19:10:30 -0400

    feat: add Spinner component with customizable class support

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { ClassValue } from 'svelte/elements';
+
+	// Prop type
+	type PropType = {
+		class?: ClassValue;
+	};
+
+	let { class: className }: PropType = $props();
+
+	const classes = [
+		'h-8 w-8 border-4 animate-spin rounded-[50%] border-solid border-primary-blue/30 border-t-primary-blue',
+		className
+	];
+</script>
+
+<span class={classes}></span>
