9f5fd70f7a244aa93c929bc27046bb4b96d6f688
NanezX <victor.nanej@gmail.com>
2025-01-29 09:18:35 -0400

    feat: update login form handling to use superforms fail method and add mock login functionality

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,13 +1,18 @@
 import { UserLoginSchema } from '$lib/utils/zod';
-import { fail } from '@sveltejs/kit';
-import { message, superValidate } from 'sveltekit-superforms';
+import { fail as failSvelte } from '@sveltejs/kit';
+import { message, superValidate, fail as failForms } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+
+// TODO: Add the message type based on the docs: https://superforms.rocks/concepts/messages
+// Basically, createa message object /type. We can add it to the globals
+// TODO: Integrate toastify for the messages
 
 export const load = async () => {
     const loginForm = await superValidate(zod(UserLoginSchema));
 
     return { loginForm };
 };
+
 
 export const actions = {
     login: async ({ request }) => {
@@ -18,11 +23,30 @@ export const actions = {
 
         if (!form.valid) {
             // Again, return { form } and things will just work.
-            return fail(400, { form });
+            return failForms(400, { form });
         }
 
-        // TODO: Do something with the validated form.data
 
-        return message(form, 'Form posted successfully!');
+        // TODO: Do something REAL with the validated form.data
+        // TODO: Redirect after succesful login. Use the current code from Lucia
+
+        return mockLogin(form, form.data.email, form.data.password)
+
     }
 };
+
+function mockLogin(form: any, email: string, password: string) {
+    const validUser = {
+        email: "admin@gmail.com",
+        password: "Admin123"
+    }
+
+
+    if (validUser.email != email.toLowerCase() || validUser.password != password) {
+        return message(form, 'Credenciales incorrectos', { status: 401 });
+    }
+
+
+    return message(form, 'Inicio de sesión exitoso');
+
+}
