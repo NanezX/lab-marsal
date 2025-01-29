fef1c20f282e950e7eff9d16a0db00e53fe6d58f
NanezX <victor.nanej@gmail.com>
2025-01-29 07:17:44 -0400

    feat: add reusable Button component with customizable properties

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { ClassValue, MouseEventHandler } from 'svelte/elements';
+
+	type PropType = {
+		children: any;
+		type?: 'submit' | 'reset' | 'button';
+		title?: string;
+		class?: ClassValue;
+		onclick?: MouseEventHandler<HTMLButtonElement>;
+	};
+
+	let { children, title: text, class: className, type = 'button', onclick }: PropType = $props();
+
+	let classes = [
+		'bg-primary-blue text-white hover:bg-primary-blue/75 px-4 py-1 rounded-3xl',
+		className
+	];
+</script>
+
+<button {type} class={classes} {onclick} title={text}>{@render children()}</button>
