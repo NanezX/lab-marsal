ffb4bf69ff1ab61473b7668a3c153d5b07386b6d
NanezX <victor.nanej@gmail.com>
2025-01-29 03:43:50 -0400

    refactor: update alt text for images on login page for better accessibility

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -10,7 +10,7 @@
 			class="from-secondary-blue to-primary-blue rounded-l-lm w-3/5 space-y-10 rounded-l-xl bg-gradient-to-br text-white"
 		>
 			<div class="flex items-center justify-around">
-				<img alt="The project logo" src={icon} width="100" height="100" />
+				<img alt="El logo de MarsalLab" src={icon} width="100" height="100" />
 
 				<h1 class="text-end text-4xl">Bienvenido a MarsalLab</h1>
 			</div>
@@ -22,7 +22,7 @@
 					ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				</p>
 
-				<img src={tree} alt="" width="250" class="mx-auto" />
+				<img src={tree} alt="Hermoso arbol al iniciar sesion" width="250" class="mx-auto" />
 			</div>
 		</div>
 		<div class="w-2/5 rounded-r-xl border bg-white">
