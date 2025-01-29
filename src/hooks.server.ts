e16ebd7803718df22d2b6f91b4cd70b9367b0981
NanezX <victor.nanej@gmail.com>
2025-01-29 01:23:04 -0400

    check if there users logged or redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,36 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
+	// Get the session token from the cookies
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
+
+	// If no session token, set locals to null
 	if (!sessionToken) {
 		event.locals.user = null;
 		event.locals.session = null;
-		return resolve(event);
-	}
-
-	const { session, user } = await auth.validateSessionToken(sessionToken);
-	if (session) {
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
 	} else {
-		auth.deleteSessionTokenCookie(event);
+		// Validate the ssesion
+		const { session, user } = await auth.validateSessionToken(sessionToken);
+
+		if (session) {
+			// Refresh the session token cookie if the session is valid
+			auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+		} else {
+			// Delete the session token cookie if the session is invalid
+			auth.deleteSessionTokenCookie(event);
+		}
+
+		// Populate locals with user and session dat
+		event.locals.user = user;
+		event.locals.session = session;
 	}
 
-	event.locals.user = user;
-	event.locals.session = session;
+	// Redirect unauthenticated users to /login
+	if (!event.locals.user && event.url.pathname !== '/login') {
+		throw redirect(302, '/login');
+	}
+
 
 	return resolve(event);
 };
