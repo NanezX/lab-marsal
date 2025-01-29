cd209c6a8d7a8f3915ffbdefe21a7f7446aa732c
NanezX <victor.nanej@gmail.com>
2025-01-29 01:40:51 -0400

    add login page layout with container component

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Container from '$lib/components/Container.svelte';
+</script>
+
+<div class="flex h-full items-center justify-center">
+	<Container>
+		<p>Let's gou</p>
+		<p>Let's gou</p>
+	</Container>
+</div>
