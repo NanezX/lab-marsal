42ce2099c12e5a03cc70e2913b2a1a66ab6ccf0b
NanezX <victor.nanej@gmail.com>
2025-01-29 16:51:52 -0400

    :art: format code for improved readability in login page server logic

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,5 +1,10 @@
 import { UserLoginSchema } from '$lib/utils/zod';
-import { message, superValidate, fail as failForms, type SuperValidated } from 'sveltekit-superforms';
+import {
+	message,
+	superValidate,
+	fail as failForms,
+	type SuperValidated
+} from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
 // import { fail as failSvelte } from '@sveltejs/kit';
@@ -33,15 +38,18 @@ export const actions = {
 	}
 };
 
-
 // TODO: This mock function and type is temporal
-type TempFormType = SuperValidated<{
-	email: string;
-	password: string;
-}, unknown, {
-	email: string;
-	password: string;
-}>
+type TempFormType = SuperValidated<
+	{
+		email: string;
+		password: string;
+	},
+	unknown,
+	{
+		email: string;
+		password: string;
+	}
+>;
 function mockLogin(form: TempFormType, email: string, password: string) {
 	const validUser = {
 		email: 'admin@gmail.com',
