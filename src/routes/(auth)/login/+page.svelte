3af6724da3f9a44644c7edf4ddc801b2d30aa3de
NanezX <victor.nanej@gmail.com>
2025-01-29 03:26:07 -0400

    refactor: update login page layout and styling

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -4,16 +4,15 @@
 	import icon from '$lib/assets/icon.png';
 </script>
 
-<div class="flex h-full items-center justify-center">
-	<Container class={['flex', 'direct-children:p-8', 'rounded-xl']}>
-		<!-- <div class="from-secondary-blue to-dark-blue via-primary-blue  rounded-l-lm w-3/5 rounded-l-xl bg-gradient-to-br"> -->
+<div class="flex h-full w-full items-center justify-center">
+	<Container class={['flex', 'direct-children:p-8', 'rounded-xl', 'w-2/3']}>
 		<div
 			class="from-secondary-blue to-primary-blue rounded-l-lm w-3/5 rounded-l-xl bg-gradient-to-br"
 		>
-			<div class="flex">
+			<div class="flex items-center gap-x-6">
 				<!-- <img alt="The project logo" src={logo} width="300" height="300" /> -->
-				<img alt="The project logo" src={icon} width="150" height="150" />
-				<h1 class="text-white">Bienvenido</h1>
+				<img alt="The project logo" src={icon} width="100" height="100" />
+				<h1 class="text-xl text-white">Bienvenido</h1>
 			</div>
 		</div>
 		<div class="w-2/5 rounded-r-xl border bg-white">
