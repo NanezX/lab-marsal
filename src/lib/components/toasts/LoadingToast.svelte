2d0477700b0c51b113dd05e3071b5d90b07c1971
NanezX <victor.nanej@gmail.com>
2025-01-29 21:06:12 -0400

    feat: add LoadingToast component with Spinner integration for loading notifications

---
+++ src/lib/components/toasts/LoadingToast.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Spinner from '../Spinner.svelte';
+
+	// Prop type
+	type PropType = {
+		text: string;
+	};
+
+	let { text }: PropType = $props();
+</script>
+
+<div class="flex items-center gap-x-4 p-2 text-lg text-black">
+	<Spinner class="max-h-6 min-h-6 min-w-6 max-w-6" />
+	<span class="break-all">{text}</span>
+</div>
