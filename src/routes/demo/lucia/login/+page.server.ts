05d7098d73be604cb14aa130e2ccf543ac7be956
NanezX <victor.nanej@gmail.com>
2025-01-31 02:06:08 -0400

    feat: assign default admin role during user registration

--- src/routes/demo/lucia/login/+page.server.ts
+++ src/routes/demo/lucia/login/+page.server.ts
@@ -76,7 +76,7 @@ export const actions: Actions = {
 		try {
 			await db
 				.insert(table.user)
-				.values({ id: userId, username, passwordHash, email: 'anyyy', fullname: 'victor' });
+				.values({ id: userId, username, passwordHash, email: 'anyyy', fullname: 'victor', role: table.UserRoles.admin });
 
 			const sessionToken = auth.generateSessionToken();
 			const session = await auth.createSession(sessionToken, userId);
