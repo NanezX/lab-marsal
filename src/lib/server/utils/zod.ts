4d4cd96e48bad40285895fdd31362472e54f03ac
NanezX <victor.nanej@gmail.com>
2025-01-31 01:58:15 -0400

    feat: add role validation to user registration schema with error handling

--- src/lib/server/utils/zod.ts
+++ src/lib/server/utils/zod.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { UserRoles } from '../db/schema';
 
 export const UserLoginSchema = z.object({
 	email: z.string().min(1, 'Correo electrónico obligatorio').email('Correo electrónico inválido'),
@@ -10,5 +11,7 @@ export const UserLoginSchema = z.object({
 
 export const UserRegisterSchema = UserLoginSchema.extend({
 	fullname: z.string().min(1, 'Nombre es obligatario'),
-	username: z.string().min(1, 'Username es obligatario')
+	username: z.string().min(1, 'Username es obligatario'),
+	// TODO: Verify that the role validation works. That also return the error message
+	role: z.nativeEnum(UserRoles, { errorMap: () => ({ message: 'Rol no valido' }) })
 });
