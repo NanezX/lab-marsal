a1fe6dae14ffc73b03496ecf03f454b5b9f46c68
NanezX <victor.nanej@gmail.com>
2025-01-31 00:59:58 -0400

    fix: add max length validation for password in UserLoginSchema

--- src/lib/server/utils/zod.ts
+++ src/lib/server/utils/zod.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const UserLoginSchema = z.object({
 	email: z.string().min(1, 'Correo electrónico obligatorio').email('Correo electrónico inválido'),
-	password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres')
+	password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres').max(20, "La contraseña debe tener máximo 20 caracteres")
 });
 
 export const UserRegisterSchema = UserLoginSchema.extend({
