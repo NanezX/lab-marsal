4918af9fc11d029acdea22fc0a1d08d78fd7bed0
NanezX <victor.nanej@gmail.com>
2025-01-31 01:28:27 -0400

    feat: implement UUID v4 based default ID generation for user table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,10 +1,20 @@
+import { encodeBase32LowerCase } from '@oslojs/encoding';
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-	id: text('id').primaryKey(),
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => {
+			// TODO: Usage of UUID v4 directly?
+			// ID with 120 bits of entropy, or about the same as UUID v4.
+			const bytes = crypto.getRandomValues(new Uint8Array(15));
+			const id = encodeBase32LowerCase(bytes);
+			return id;
+		}),
 	username: text('username').notNull().unique(),
 	email: text('email').notNull().unique(),
-	passwordHash: text('password_hash').notNull()
+	passwordHash: text('password_hash').notNull(),
+	fullname: text('fullname').notNull()
 });
 
 export const session = pgTable('session', {
