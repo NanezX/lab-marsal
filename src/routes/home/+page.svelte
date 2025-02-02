4b19f498d15155994e59d438edd318e8bb53a057
NanezX <victor.nanej@gmail.com>
2025-02-02 17:40:44 -0400

    wip

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,9 +2,18 @@
 	import Container from '$lib/components/Container.svelte';
 </script>
 
-<div class="flex h-full items-center justify-center">
-	<Container>
-		<p>Let's gou</p>
-		<p>Let's gou</p>
-	</Container>
-</div>
+<Container
+	class={[
+		'flex',
+		'direct-children:p-8',
+		'rounded-xl',
+		'w-2/3',
+		'max-w-full',
+		'bg-white',
+		'h-[600px]',
+		'items-center justify-center'
+	]}
+>
+	<p>Let's gou</p>
+	<p>Let's gou</p>
+</Container>
