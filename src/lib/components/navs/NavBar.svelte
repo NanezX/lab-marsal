ef1a9eca7995da17cf6ba817c82729c0481e10e8
NanezX <victor.nanej@gmail.com>
2025-02-02 17:53:33 -0400

    wip: nav bar

---
+++ src/lib/components/navs/NavBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import icon from '$lib/assets/icon.png';
+</script>
+
+<!-- class="absolute left-0 top-0 flex w-full justify-between bg-secondary-blue px-20 py-6 shadow-xl" -->
+<div class="absolute left-0 top-0 w-full bg-secondary-blue/80 py-4 shadow-xl">
+	<div class="flex justify-between px-40">
+		<a href="/home" title="Ir a inicio">
+			<div class="flex items-center gap-x-2">
+				<img alt="El logo de MarsalLab" src={icon} width="50" height="50" />
+				<p class="font-bold">Lab Marsal</p>
+			</div>
+		</a>
+		<p>Usuario</p>
+	</div>
+</div>
