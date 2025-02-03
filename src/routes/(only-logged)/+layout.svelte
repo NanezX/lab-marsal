76a7308090e18ef31897b184000602b87f02e572
NanezX <victor.nanej@gmail.com>
2025-02-02 22:51:54 -0400

    adding nav bar

--- src/routes/(only-logged)/+layout.svelte
+++ src/routes/(only-logged)/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	import NavBar from '$lib/components/navs/NavBar.svelte';
 	import { toastSucces } from '$lib/toasts';
 	import { SvelteToast, type SvelteToastOptions } from '@zerodevx/svelte-toast';
 
-	let { children } = $props();
+	let { children, data } = $props();
 
 	const options: SvelteToastOptions = {
 		duration: 5000,
@@ -10,4 +11,5 @@
 	};
 </script>
 
+<NavBar fullName={`${data.user.name} ${data.user.lastName}`} email={data.user.email} />
 {@render children()}
