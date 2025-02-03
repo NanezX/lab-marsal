fb902c25a29dbf41f42a947223342019e0236677
NanezX <victor.nanej@gmail.com>
2025-02-02 22:54:45 -0400

    shared container

--- src/routes/(only-logged)/+layout.svelte
+++ src/routes/(only-logged)/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Container from '$lib/components/Container.svelte';
 	import NavBar from '$lib/components/navs/NavBar.svelte';
 	import { toastSucces } from '$lib/toasts';
 	import { SvelteToast, type SvelteToastOptions } from '@zerodevx/svelte-toast';
@@ -12,4 +13,6 @@
 </script>
 
 <NavBar fullName={`${data.user.name} ${data.user.lastName}`} email={data.user.email} />
-{@render children()}
+<Container class={['flex', 'direct-children:p-8', 'rounded-xl', 'w-2/3', 'max-w-5xl']}>
+	{@render children()}
+</Container>
