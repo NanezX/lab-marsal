7ff92de8fdf358327cd2b62a071ea67c24febc42
NanezX <victor.nanej@gmail.com>
2025-02-02 23:45:04 -0400

    layout with links

--- src/routes/(only-logged)/+layout.svelte
+++ src/routes/(only-logged)/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
 	import NavBar from '$lib/components/navs/NavBar.svelte';
+	import { ChevronDown, UserCircle, Logout } from '@steeze-ui/tabler-icons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 
 	let { children, data } = $props();
 </script>
@@ -8,14 +10,37 @@
 <div class="h-full w-full">
 	<NavBar fullName={`${data.user.name} ${data.user.lastName}`} email={data.user.email} />
 	<div class="flex">
-		<div class="ml-10 mt-8 flex flex-col rounded-xl p-4 direct-children:p-2">
+		<div
+			class={[
+				'ml-10 mt-8 flex flex-col rounded-xl border bg-gray-200 p-4 text-xl shadow-2xl',
+				'direct-children:flex direct-children:items-center direct-children:gap-x-3 direct-children:p-2'
+			]}
+		>
 			<!-- Links / Modulos-->
-			<a href="/home">Inicio</a>
-			<a href="/users">Examenes</a>
-			<a href="/users">Clientes</a>
-			<a href="/users">Tipos examenes</a>
-			<a href="/users">Gestion de usuarios</a>
-			<a href="/users">Configuracion</a>
+			<a href="/home">
+				<Icon src={UserCircle} size="24" />
+				<span> Inicio </span>
+			</a>
+			<a href="/exams">
+				<Icon src={UserCircle} size="24" />
+				<span> Examenes </span>
+			</a>
+			<a href="/clients">
+				<Icon src={UserCircle} size="24" />
+				<span>Clientes</span>
+			</a>
+			<a href="/exam-types">
+				<Icon src={UserCircle} size="24" />
+				<span> Tipos examenes </span>
+			</a>
+			<a href="/users">
+				<Icon src={UserCircle} size="24" />
+				<span> Gestion de usuarios </span>
+			</a>
+			<a href="/config">
+				<Icon src={UserCircle} size="24" />
+				<span> Configuracion </span>
+			</a>
 		</div>
 		<Container class={['mt-8 flex w-2/3 max-w-5xl rounded-xl direct-children:p-8']}>
 			{@render children()}
