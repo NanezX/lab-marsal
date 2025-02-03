f4697c148fb866e4b82b8013f282d962a2ff0f8d
NanezX <victor.nanej@gmail.com>
2025-02-03 03:00:07 -0400

    lint

--- src/lib/shared/utils.ts
+++ src/lib/shared/utils.ts
@@ -2,12 +2,11 @@ export function isObject(value: unknown): value is Record<string, unknown> {
 	return typeof value === 'object' && value !== null && !Array.isArray(value);
 }
 
-
 /**
  * Format a text to capital letter
  * @param text The text to format, eg: "helLo WorLD!"
  * @returns The texted formatted with capital letter, eg: "Hello world!"
  */
 export function formatCapital(text: string): string {
-	return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+	return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
