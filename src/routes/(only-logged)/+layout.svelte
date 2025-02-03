dd6b3c1800282d41c09acee32f276f55908b3959
NanezX <victor.nanej@gmail.com>
2025-02-02 23:34:40 -0400

    refactor layout structure to enhance navigation and content display

--- src/routes/(only-logged)/+layout.svelte
+++ src/routes/(only-logged)/+layout.svelte
@@ -5,7 +5,20 @@
 	let { children, data } = $props();
 </script>
 
-<NavBar fullName={`${data.user.name} ${data.user.lastName}`} email={data.user.email} />
-<Container class={['flex', 'direct-children:p-8', 'rounded-xl', 'w-2/3', 'max-w-5xl']}>
-	{@render children()}
-</Container>
+<div class="h-full w-full">
+	<NavBar fullName={`${data.user.name} ${data.user.lastName}`} email={data.user.email} />
+	<div class="flex">
+		<div class="ml-10 mt-8 flex flex-col rounded-xl p-4 direct-children:p-2">
+			<!-- Links / Modulos-->
+			<a href="/home">Inicio</a>
+			<a href="/users">Examenes</a>
+			<a href="/users">Clientes</a>
+			<a href="/users">Tipos examenes</a>
+			<a href="/users">Gestion de usuarios</a>
+			<a href="/users">Configuracion</a>
+		</div>
+		<Container class={['mt-8 flex w-2/3 max-w-5xl rounded-xl direct-children:p-8']}>
+			{@render children()}
+		</Container>
+	</div>
+</div>
