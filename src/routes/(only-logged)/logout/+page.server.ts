0810c5009da8946e18f54938319d4729d9b3e265
NanezX <victor.nanej@gmail.com>
2025-02-03 01:03:04 -0400

    lint

--- src/routes/(only-logged)/logout/+page.server.ts
+++ src/routes/(only-logged)/logout/+page.server.ts
@@ -3,13 +3,13 @@ import * as auth from '$lib/server/auth';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-    default: async (event) => {
-        if (!event.locals.session) {
-            return error(401);
-        }
+	default: async (event) => {
+		if (!event.locals.session) {
+			return error(401);
+		}
 
-        await auth.invalidateSession(event.locals.session.id);
-        auth.deleteSessionTokenCookie(event);
-        return redirect(302, '/login');
-    }
+		await auth.invalidateSession(event.locals.session.id);
+		auth.deleteSessionTokenCookie(event);
+		return redirect(302, '/login');
+	}
 };
