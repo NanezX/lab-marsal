f8790f1b9988cf4299e741708cfc1778a9396a19
NanezX <victor.nanej@gmail.com>
2025-02-02 22:45:55 -0400

    wip

---
+++ src/routes/(only-logged)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { toastSucces } from '$lib/toasts';
+	import { SvelteToast, type SvelteToastOptions } from '@zerodevx/svelte-toast';
+
+	let { children } = $props();
+
+	const options: SvelteToastOptions = {
+		duration: 5000,
+		pausable: true
+	};
+</script>
+
+{@render children()}
