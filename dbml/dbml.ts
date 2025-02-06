39b461b3445565635e621d6c5575632f2e5639ff
NanezX <victor.nanej@gmail.com>
2025-02-06 03:02:38 -0400

    move dbml script to single folder

--- dbml/dbml.ts
+++ dbml/dbml.ts
@@ -1,7 +1,7 @@
-import * as schema from './src/lib/server/db/schema';
+import * as schema from '../src/lib/server/db/schema';
 import { pgGenerate } from 'drizzle-dbml-generator'; // Using Postgres for this example
 
-const out = './schema2.dbml';
+const out = './dbml/schema.dbml';
 const relational = true;
 
 pgGenerate({ schema, out, relational });
