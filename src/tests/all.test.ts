63a79df10b9bbe42ee1b875a6742a9adf5249046
NanezX <victor.nanej@gmail.com>
2025-02-06 20:42:01 -0400

    mocked async

--- src/tests/all.test.ts
+++ src/tests/all.test.ts
@@ -1,98 +1,122 @@
 import { describe, it, expect } from 'vitest';
+import { delay } from './utils';
 
 describe('Auth sessions', () => {
-	it('should generate the session token correctly', () => {
+	it('should generate the session token correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should create the session correctly', () => {
+	it('should create the session correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate the session token and not obtain user if no session found', () => {
+	it('should validate the session token and not obtain user if no session found', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate the session token and not obtain user if the session expired', () => {
+	it('should validate the session token and not obtain user if the session expired', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate the session token and get the user at valid session', () => {
+	it('should validate the session token and get the user at valid session', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate the session token and extend the session', () => {
+	it('should validate the session token and extend the session', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should soft delete the session with invalidateSession', () => {
+	it('should soft delete the session with invalidateSession', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should set the session token at the cookies correctly', () => {
+	it('should set the session token at the cookies correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should delete the session token at the cookies correctly', () => {
+	it('should delete the session token at the cookies correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 });
 
 describe('Rol authorization', () => {
-	it('should get the role of the user after validating the session', () => {
+	it('should get the role of the user after validating the session', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
-	it('should fail to get the role of the user with invalid session', () => {
+	it('should fail to get the role of the user with invalid session', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 });
 
 describe('Zod input validations', () => {
-	it('should validate user login data with the user login schema correctly', () => {
+	it('should validate user login data with the user login schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong user login data with the user login schema', () => {
+	it('should return error with wrong user login data with the user login schema', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate user register data with the user register schema correctly', () => {
+	it('should validate user register data with the user register schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong user register data with the user register schema', () => {
+	it('should return error with wrong user register data with the user register schema', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate patient register data with the patient register schema correctly', () => {
+	it('should validate patient register data with the patient register schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong patient register data with the patient register schema ', () => {
+	it('should return error with wrong patient register data with the patient register schema ', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate exam type creation data with the exam type creation schema correctly', () => {
+	it('should validate exam type creation data with the exam type creation schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong exam type creation data with the exam type creation schema', () => {
+	it('should return error with wrong exam type creation data with the exam type creation schema', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate exam creation data with the exam creation schema correctly', () => {
+	it('should validate exam creation data with the exam creation schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong exam creation data with the exam creation schema', () => {
+	it('should return error with wrong exam creation data with the exam creation schema', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should validate configuration data with the configuration schema correctly', () => {
+	it('should validate configuration data with the configuration schema correctly', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should return error with wrong configuration data with the configuration schema', () => {
+	it('should return error with wrong configuration data with the configuration schema', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 });
--- src/tests/all2.test.ts
+++ src/tests/all2.test.ts
@@ -1,160 +1,198 @@
 import { describe, it, expect } from 'vitest';
+import { delay } from './utils';
 
-describe('Utilities', () => {
-	it('should detect that value is an object using isObject', () => {
+describe('Utilities', async () => {
+	it('should detect that value is an object using isObject', async () => {
+		expect(true).toBeTruthy();
+		await delay(4000);
+	});
+
+	it('should detect that value is NOT an object using isObject', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should detect that value is NOT an object using isObject', () => {
+	it('should format to capital the text correclty', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should format to capital the text correclty', () => {
+	it('should organize the arrays inside of an object for a given key', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should organize the arrays inside of an object for a given key', () => {
+	it('should format an ISO String to a human readable date', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should format an ISO String to a human readable date', () => {
+	it('should hash the text', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 
-	it('should hash the text', () => {
-		expect(true).toBeTruthy();
-	});
-
-	it('should verify the hashed text', () => {
+	it('should verify the hashed text', async () => {
+		await delay(4000);
 		expect(true).toBeTruthy();
 	});
 });
 
-describe('Database operations', () => {
-	describe('Configuration table', () => {
-		it('should be able to insert a new configuration row', () => {
+describe('Database operations', async () => {
+	describe('Configuration table', async () => {
+		it('should be able to insert a new configuration row', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to update a configuration row', () => {
+		it('should be able to update a configuration row', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get all the rows', () => {
+		it('should be able to get all the rows', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to delete a configuration row', () => {
+		it('should be able to delete a configuration row', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 	});
 
-	describe('Session table', () => {
-		it('should be able to insert a new session', () => {
+	describe('Session table', async () => {
+		it('should be able to insert a new session', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to update a session', () => {
+		it('should be able to update a session', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get all the sessions', () => {
+		it('should be able to get all the sessions', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to find a specific session', () => {
+		it('should be able to find a specific session', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to delete a session', () => {
+		it('should be able to delete a session', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should only accept sessions related to an user', () => {
+		it('should only accept sessions related to an user', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 	});
 
-	describe('User table', () => {
-		it('should be able to insert a new user', () => {
+	describe('User table', async () => {
+		it('should be able to insert a new user', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to update an user', () => {
+		it('should be able to update an user', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get all the users', () => {
+		it('should be able to get all the users', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to soft delete an user', () => {
+		it('should be able to soft delete an user', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should fail to insert an user with an already used document id', () => {
+		it('should fail to insert an user with an already used document id', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
-		it('should fail to update an user to an already used document id', () => {
+		it('should fail to update an user to an already used document id', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should fail to insert an user with an already used email', () => {
+		it('should fail to insert an user with an already used email', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should fail to update an user to an already used email', () => {
+		it('should fail to update an user to an already used email', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 	});
 
-	describe('Patient table', () => {
-		it('should be able to insert a new patient', () => {
+	describe('Patient table', async () => {
+		it('should be able to insert a new patient', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to update an patient', () => {
+		it('should be able to update an patient', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get all the patients', () => {
+		it('should be able to get all the patients', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get a specific patient', () => {
+		it('should be able to get a specific patient', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to soft delete an patient', () => {
+		it('should be able to soft delete an patient', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should fail to insert an patient with an already used document id', () => {
+		it('should fail to insert an patient with an already used document id', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
-		it('should fail to update an patient to an already used document id', () => {
+		it('should fail to update an patient to an already used document id', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 	});
 
-	describe('Exam table', () => {
-		it('should be able to insert a new exam', () => {
+	describe('Exam table', async () => {
+		it('should be able to insert a new exam', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to update an exam', () => {
+		it('should be able to update an exam', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get all the exams', () => {
+		it('should be able to get all the exams', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to get a specific exam', () => {
+		it('should be able to get a specific exam', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 
-		it('should be able to soft delete an exam', () => {
+		it('should be able to soft delete an exam', async () => {
+			await delay(4000);
 			expect(true).toBeTruthy();
 		});
 	});
