bcd7db9f2a99fce883da6c729ba66ab86f50d63c
NanezX <victor.nanej@gmail.com>
2025-02-06 20:22:30 -0400

    remove commented code

--- src/routes/(authenticated)/register/+page.server.ts
+++ src/routes/(authenticated)/register/+page.server.ts
@@ -7,30 +7,6 @@ import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
 import type { Actions } from './$types';
 import postgres from 'postgres';
-// import * as auth from '$lib/server/auth';
-
-// import * as auth from '$lib/server/auth';
-// import { fail, redirect } from '@sveltejs/kit';
-// import type { Actions, PageServerLoad } from './$types';
-
-// export const load: PageServerLoad = async (event) => {
-// 	if (!event.locals.user) {
-// 		return redirect(302, '/demo/lucia/login');
-// 	}
-// 	return { user: event.locals.user };
-// };
-
-// export const actions: Actions = {
-// 	logout: async (event) => {
-// 		if (!event.locals.session) {
-// 			return fail(401);
-// 		}
-// 		await auth.invalidateSession(event.locals.session.id);
-// 		auth.deleteSessionTokenCookie(event);
-
-// 		return redirect(302, '/demo/lucia/login');
-// 	}
-// };
 
 export const load = async () => {
 	const registerForm = await superValidate(zod(UserRegisterSchema));
