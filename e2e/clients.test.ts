cad535742f21978ec4bc68dc84a5617329890cfe
NanezX <victor.nanej@gmail.com>
2025-02-06 22:23:43 -0400

    tests

--- e2e/clients.test.ts
+++ e2e/clients.test.ts
@@ -1,32 +1,32 @@
 import { expect, test } from '@playwright/test';
 
-test.describe(('Exam page'), () => {
+test.describe(('Client page'), () => {
 
-    test('should display the search bar for exams', async ({ page }) => {
+    test('should display the search bar for clients', async ({ page }) => {
         await page.goto('/login');
         await expect(page.locator('h1')).toBeVisible();
     });
-    test('should display exams on the page', async ({ page }) => {
-        await page.goto('/login');
-        await expect(page.locator('h1')).toBeVisible();
-    });
-
-    test('should filter the displayed exams using the search bar for exams', async ({ page }) => {
+    test('should display client on the page', async ({ page }) => {
         await page.goto('/login');
         await expect(page.locator('h1')).toBeVisible();
     });
 
-    test('should allow to redirect to specific exam page when clicking to the document icon', async ({ page }) => {
+    test('should filter the displayed client using the search bar for clients', async ({ page }) => {
         await page.goto('/login');
         await expect(page.locator('h1')).toBeVisible();
     });
 
-    test('should allow to modify specific exam when clicking to the modify icon', async ({ page }) => {
+    test('should allow to redirect to specific client page when clicking to the client icon', async ({ page }) => {
         await page.goto('/login');
         await expect(page.locator('h1')).toBeVisible();
     });
 
-    test('should allow to delete specific exam when clicking to the delete icon', async ({ page }) => {
+    test('should allow to modify specific client exam when clicking to the modify icon', async ({ page }) => {
+        await page.goto('/login');
+        await expect(page.locator('h1')).toBeVisible();
+    });
+
+    test('should allow to delete specific client when clicking to the delete icon', async ({ page }) => {
         await page.goto('/login');
         await expect(page.locator('h1')).toBeVisible();
     });
