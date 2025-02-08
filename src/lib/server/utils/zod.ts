4db0e2d34fe3af93070d34bb43be8c22a59b2a88
NanezX <victor.nanej@gmail.com>
2025-02-08 18:42:40 -0400

    feat: enhance UserRegisterSchema with document ID and birthdate validation

--- src/lib/server/utils/zod.ts
+++ src/lib/server/utils/zod.ts
@@ -1,4 +1,5 @@
 import { UserRoles } from '$lib/shared/enums';
+import { minDocumentId, maxDocumentId } from '$lib/shared/utils';
 import { z } from 'zod';
 
 export const UserLoginSchema = z.object({
@@ -10,10 +11,27 @@ export const UserLoginSchema = z.object({
 });
 
 export const UserRegisterSchema = UserLoginSchema.extend({
-	name: z.string().min(1, 'Nombre es obligatario'),
+	repeatPassword: z.string().min(1, 'Debe repetir la contraseña'),
+	firstName: z.string().min(1, 'Nombre es obligatario'),
 	lastName: z.string().min(1, 'Apellido es obligatario'),
 	role: z.nativeEnum(UserRoles, { errorMap: () => ({ message: 'Rol no valido' }) }),
-	repeatPassword: z.string().min(1, 'Debe repetir la contraseña')
+	documentId: z
+		.number()
+		.min(minDocumentId, 'Solo números positivos pueden ser cédula')
+		.max(maxDocumentId, 'Número muy grande para ser una cédula'),
+	birthdate: z
+		.string()
+		.min(1, 'Debe ingresar una fecha')
+		.date('No es una fecha valida')
+		.refine((value_) => {
+			// Get the current date
+			const minAgeDate = new Date();
+			//  Set full date minus 18 years
+			minAgeDate.setFullYear(minAgeDate.getFullYear() - 18);
+			// Get a new date instance using the passed date
+			const inputDate = new Date(value_);
+			return inputDate <= minAgeDate;
+		}, 'Debe tener mínimo 18 años')
 }).refine((obj) => obj.password === obj.repeatPassword, {
 	message: 'Las contraseñas no coinciden',
 	path: ['repeatPassword']
