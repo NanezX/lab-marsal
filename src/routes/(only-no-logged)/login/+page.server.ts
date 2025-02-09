646127a0a70ced73951c040f592bbaca35ac16aa
NanezX <victor.nanej@gmail.com>
2025-02-09 01:43:50 -0400

    feat: refactor login logic to use findUserByEmail and streamline password verification

--- src/routes/(only-no-logged)/login/+page.server.ts
+++ src/routes/(only-no-logged)/login/+page.server.ts
@@ -3,11 +3,10 @@ import { superValidate, fail as failForms, message } from 'sveltekit-superforms'
 import { zod } from 'sveltekit-superforms/adapters';
 import { verify } from '@node-rs/argon2';
 import { redirect } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
 import * as auth from '$lib/server/auth';
-import { db } from '$lib/server/db';
-import * as table from '$lib/server/db/schema';
 import type { Actions } from './$types';
+import { findUserByEmail } from '$lib/server/utils/dbQueries';
+import { hashingOptions } from '$lib/server/utils';
 
 export const load = async () => {
 	const loginForm = await superValidate(zod(UserLoginSchema));
@@ -25,38 +24,30 @@ export const actions: Actions = {
 			return failForms(400, { form });
 		}
 
+		// Obtain all the data from the login form
 		const { email, password } = form.data;
 
-		const results = await db
-			.select()
-			.from(table.user)
-			.where(eq(table.user.email, email.toLowerCase()));
-
-		const existingUser = results.at(0);
-
+		// Query for user with the email
+		const existingUser = await findUserByEmail(email.toLowerCase(), 'deleted');
 		if (!existingUser) {
 			return message(
 				form,
-				{ text: 'Incorrect username or password', type: 'error' },
-				{ status: 400 }
+				{ text: 'Correo o contraseña inválida', type: 'error' },
+				{ status: 401 }
 			);
 		}
 
-		const validPassword = await verify(existingUser.passwordHash, password, {
-			memoryCost: 19456,
-			timeCost: 2,
-			outputLen: 32,
-			parallelism: 1
-		});
-
+		// Verify password entered
+		const validPassword = await verify(existingUser.passwordHash, password, hashingOptions);
 		if (!validPassword) {
 			return message(
 				form,
-				{ text: 'Incorrect username or password', type: 'error' },
-				{ status: 400 }
+				{ text: 'Correo o contraseña inválida', type: 'error' },
+				{ status: 401 }
 			);
 		}
 
+		// Set the session
 		const sessionToken = auth.generateSessionToken();
 		const session = await auth.createSession(sessionToken, existingUser.id);
 		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
