fbdd6350f33c934e275ecbdbff1a52b42742967b
NanezX <victor.nanej@gmail.com>
2025-02-09 03:07:27 -0400

    fix: update login logic to prevent login for soft-deleted users

--- src/routes/(only-no-logged)/login/+page.server.ts
+++ src/routes/(only-no-logged)/login/+page.server.ts
@@ -28,9 +28,8 @@ export const actions: Actions = {
 		const { email, password } = form.data;
 
 		// Query for user with the email
-		// TODO: Support for soft deleted. If user is soft deleted, should not let login with it AND also not register it
-		const existingUser = await findUserByEmail(email.toLowerCase(), 'deleted');
-		if (!existingUser) {
+		const existingUser = await findUserByEmail(email.toLowerCase());
+		if (!existingUser || existingUser.deleted) {
 			return message(
 				form,
 				{ text: 'Correo o contraseña inválida', type: 'error' },
