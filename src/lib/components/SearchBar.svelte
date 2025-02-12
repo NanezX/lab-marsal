249f7968711581c4f3a3096da4442ca251672b35
NanezX <victor.nanej@gmail.com>
2025-02-12 17:38:54 -0400

    wip: search bar

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Input from './Input.svelte';
+	import { Search } from '@steeze-ui/tabler-icons';
+	import type { ClassValue } from 'svelte/elements';
+
+	type PropType = {
+		inputSearch: string | number;
+		name: string;
+		placeholder: string;
+		class?: ClassValue;
+		wrapperClass?: ClassValue;
+	};
+
+	let {
+		inputSearch = $bindable(''),
+		name,
+		placeholder,
+		class: enteredClass,
+		wrapperClass: wrapperClass
+	}: PropType = $props();
+</script>
+
+<Input
+	bind:value={inputSearch}
+	{name}
+	{placeholder}
+	icon={Search}
+	class={enteredClass}
+	{wrapperClass}
+/>
