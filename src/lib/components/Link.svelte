a4d8525797326e680223c9c319973798ecf0d319
NanezX <victor.nanej@gmail.com>
2025-02-12 17:57:26 -0400

    Link component with similar class than the button

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { ClassValue } from 'svelte/elements';
+
+	type PropType = {
+		children: Snippet;
+		href: string;
+		title?: string;
+		class?: ClassValue;
+	};
+
+	let { children, href, title, class: className }: PropType = $props();
+</script>
+
+<a
+	class={[
+		'bg-primary-blue hover:bg-primary-blue/75 cursor-pointer rounded-3xl px-4 py-1 text-white',
+		'disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-600',
+		className
+	]}
+	{title}
+	{href}
+>
+	{@render children()}
+</a>
