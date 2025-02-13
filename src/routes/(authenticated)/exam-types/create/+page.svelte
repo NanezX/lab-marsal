c6d82a1a28946bac5eb27e21d4f47d71be3488ca
NanezX <victor.nanej@gmail.com>
2025-02-12 21:23:47 -0400

    wip: create exam type page

--- src/routes/(authenticated)/exam-types/create/+page.svelte
+++ src/routes/(authenticated)/exam-types/create/+page.svelte
@@ -1,1 +1,47 @@
-<p>Crear nuevo examen</p>
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+	import Textarea from '$lib/components/Textarea.svelte';
+	import { fade } from 'svelte/transition';
+
+	let examName = $state('');
+	let examDescription = $state('');
+	let basePrice = $state('');
+</script>
+
+<div in:fade class="flex w-full flex-col gap-y-8">
+	<p class="text-center text-3xl">Crear tipo de exámen</p>
+
+	<div>
+		<div class="space-y-5">
+			<p class="text-xl">Detalles generales</p>
+
+			<div class="space-y-4">
+				<div class="flex gap-x-8">
+					<Input
+						bind:value={examName}
+						name="name"
+						placeholder="Nombre del exámen"
+						wrapperClass="w-1/2"
+					/>
+
+					<Input value={examName} name="basePrice" placeholder="Precio base" wrapperClass="w-1/3" />
+				</div>
+				<Textarea
+					bind:value={examDescription}
+					name="description"
+					placeholder="Description del exámen"
+				/>
+			</div>
+		</div>
+
+		<hr class="border-primary-gray/50 my-4" />
+
+		<div class="space-y-5">
+			<p class="text-xl">Valores y parámetros</p>
+
+			<div class="space-y-4">
+				<Input bind:value={examName} name="name" placeholder="Nombre del exámen" />
+			</div>
+		</div>
+	</div>
+</div>
