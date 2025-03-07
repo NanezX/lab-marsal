5a7f6fe4db101a82cd04507da4f867dac79c4f90
NanezX <victor.nanej@gmail.com>
2025-03-07 03:47:08 -0400

    refactor: update pagination controls for improved layout and accessibility

--- src/lib/components/FilterControls.svelte
+++ src/lib/components/FilterControls.svelte
@@ -32,16 +32,20 @@
 </script>
 
 <div
-	class="direct-children:hover:text-blue-700 direct-children:hover:underline mx-auto w-fit space-x-2 text-lg font-semibold"
+	class="direct-children:hover:text-blue-700 direct-children:hover:underline mx-auto flex w-fit items-center gap-x-2 text-lg font-semibold"
 >
 	<!-- Back -->
 	<a
+		title="Anterior"
 		href={generateHref(currentPage - 1)}
-		class={{
-			'pointer-events-none opacity-50': currentPage === 0
-		}}
+		class={[
+			'mt-0.5',
+			{
+				'pointer-events-none opacity-50': currentPage === 0
+			}
+		]}
 	>
-		<Icon src={ArrowLeft} size="24" />
+		<Icon src={ArrowLeft} size="20" />
 	</a>
 
 	<!-- Pagination Numbers (Example for pages 1, 2, 3, 4) -->
@@ -58,11 +62,15 @@
 
 	<!-- Forward -->
 	<a
+		title="Siguiente"
 		href={generateHref(currentPage + 1)}
-		class={{
-			'pointer-events-none opacity-50': totalPages < 1 || currentPage === totalPages - 1
-		}}
+		class={[
+			'mt-0.5',
+			{
+				'pointer-events-none opacity-50': totalPages < 1 || currentPage === totalPages - 1
+			}
+		]}
 	>
-		<Icon src={ArrowRight} size="24" />
+		<Icon src={ArrowRight} size="20" />
 	</a>
 </div>
