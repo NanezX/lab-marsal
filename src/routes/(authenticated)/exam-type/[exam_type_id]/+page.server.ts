53a7c50e679c683bbbae4d13f659d3c4e2099b64
NanezX <victor.nanej@gmail.com>
2025-05-12 23:19:22 -0400

    feat: add validation for exam type ID and handle errors in load function

--- src/routes/(authenticated)/exam-type/[exam_type_id]/+page.server.ts
+++ src/routes/(authenticated)/exam-type/[exam_type_id]/+page.server.ts
@@ -1,8 +1,17 @@
 import type { PageServerLoad } from './$types';
 import { findExamTypeById } from '$lib/server/utils/dbQueries';
-
+import { error as svelteError } from '@sveltejs/kit';
+import { validate } from 'uuid';
 export const load: PageServerLoad = async ({ params }) => {
+	if (!validate(params.exam_type_id)) {
+		svelteError(404, 'Exam type ID not valid');
+	}
+	
 	const examTypeData = await findExamTypeById(params.exam_type_id);
+
+	if (examTypeData == undefined) {
+		svelteError(404, 'Exam type not found');
+	}
 
 	return { examTypeData };
 };
