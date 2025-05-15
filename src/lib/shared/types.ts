6a00513d28c2bdf715aae9662893e979f4d09b5c
NanezX <victor.nanej@gmail.com>
2025-05-14 22:15:55 -0400

    fix: export ExamTypeWithParameters type for external usage

--- src/lib/shared/types.ts
+++ src/lib/shared/types.ts
@@ -1,7 +1,7 @@
 import type { findExamTypeById } from '$lib/server/utils/dbQueries';
 
 // Get the return type of `findExamTypeById`
-type ExamTypeWithParameters = NonNullable<Awaited<ReturnType<typeof findExamTypeById>>>;
+export type ExamTypeWithParameters = NonNullable<Awaited<ReturnType<typeof findExamTypeById>>>;
 
 // Extract the `Parameter` type from the `parameters` array
 export type Parameter = ExamTypeWithParameters['parameters'][number];
