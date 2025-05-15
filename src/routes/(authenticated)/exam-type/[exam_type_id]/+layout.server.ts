6cd9b2d5fe64e8bae6c776f06a14109188eb9e2d
NanezX <victor.nanej@gmail.com>
2025-05-14 21:51:24 -0400

    move load exam type id into layout

--- src/routes/(authenticated)/exam-type/[exam_type_id]/+layout.server.ts
+++ src/routes/(authenticated)/exam-type/[exam_type_id]/+layout.server.ts
@@ -1,15 +1,16 @@
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 import { findExamTypeById } from '$lib/server/utils/dbQueries';
 import { error as svelteError } from '@sveltejs/kit';
 import { validate } from 'uuid';
-export const load: PageServerLoad = async ({ params }) => {
+
+export const load: LayoutServerLoad = async ({ params }) => {
 	if (!validate(params.exam_type_id)) {
 		svelteError(404, 'Exam type ID not valid');
 	}
 
 	const examTypeData = await findExamTypeById(params.exam_type_id);
 
-	if (examTypeData == undefined) {
+	if (!examTypeData) {
 		svelteError(404, 'Exam type not found');
 	}
 
---
+++ src/routes/(authenticated)/exam-type/[exam_type_id]/edit/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	import Button from '$lib/components/Button.svelte';
+	import { fade } from 'svelte/transition';
+	import { sortArrayObject } from '$lib/shared/utils';
+	import DisplayExamTypeParams from '$lib/components/DisplayExamTypeParams.svelte';
+	import { generatePDF } from '$lib/client';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { PencilMinus } from '@steeze-ui/tabler-icons';
+
+	let { data }: PageProps = $props();
+
+	let { examTypeData } = data;
+</script>
