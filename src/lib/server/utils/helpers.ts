de353bafe96c381b88597df025c5b6b8d3aaf9aa
NanezX <victor.nanej@gmail.com>
2025-05-15 01:35:30 -0400

    refactor: improve isUniqueConstraintViolation function by adding error code check for unique constraint violations

--- src/lib/server/utils/helpers.ts
+++ src/lib/server/utils/helpers.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
 
 export function isUniqueConstraintViolation(err: unknown, constraint?: string): boolean {
-	if (err instanceof postgres.PostgresError) {
+	// '23505' is the PostgreSQL error code for unique violation
+	if (err instanceof postgres.PostgresError && err.code === '23505') {
 		if (!constraint) return true;
 		return err.constraint_name === constraint;
 	}
