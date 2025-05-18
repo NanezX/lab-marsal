e9d3579b3828daa38c2dc43cd42a898e0894152e
NanezX <victor.nanej@gmail.com>
2025-05-18 11:06:49 -0400

    added todo

--- src/routes/(authenticated)/exam-types/+page.svelte
+++ src/routes/(authenticated)/exam-types/+page.svelte
@@ -26,6 +26,8 @@
 	}
 </script>
 
+<!-- TODO: Improve the UI of the type exam list  -->
+
 <div in:fade class="flex w-full flex-col gap-y-8">
 	<p class="text-center text-3xl">Tipos de exámenes</p>
 
