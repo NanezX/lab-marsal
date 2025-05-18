bbf953d1fb361ab9b1c4b397e7ac793699f57d9d
NanezX <victor.nanej@gmail.com>
2025-05-18 17:15:40 -0400

    wip

---
+++ src/lib/components/modal/NavigationGuard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { beforeNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	type PropType = {
+		shouldBlock: () => boolean;
+		onBlock: (continueNav: () => void) => void;
+	};
+
+	let { shouldBlock, onBlock }: PropType = $props();
+
+	let nextUrl: string | null = null;
+
+	onMount(() => {
+		const unsubscribe = beforeNavigate((nav) => {
+			if (shouldBlock() && nav.to?.url) {
+				nav.cancel(); // stop the navigation
+				nextUrl = nav.to.url.pathname + nav.to.url.search + nav.to.url.hash;
+
+				onBlock(() => {
+					if (nextUrl) {
+						console.log('nextUrl: ', nextUrl);
+						goto(nextUrl);
+						nextUrl = null;
+					}
+				});
+			}
+		});
+
+		return unsubscribe;
+	});
+</script>
