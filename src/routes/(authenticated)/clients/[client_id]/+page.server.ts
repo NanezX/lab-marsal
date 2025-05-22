8455afc90c709aede7e32843971a16f94a443024
NanezX <victor.nanej@gmail.com>
2025-05-22 17:23:44 -0400

    refactor: streamline patient exams retrieval and remove unused code

--- src/routes/(authenticated)/clients/[client_id]/+page.server.ts
+++ src/routes/(authenticated)/clients/[client_id]/+page.server.ts
@@ -4,7 +4,7 @@ import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { deletePatientSchema } from '$lib/server/utils/zod';
 import type { PageServerLoad } from './$types';
-import { count, eq, and, desc } from 'drizzle-orm';
+import { count, eq, and } from 'drizzle-orm';
 
 // TODO: Verify what roles can delete an patient (on the action) - (maybe just block the page to those user in the backend)
 
@@ -24,33 +24,31 @@ export const load: PageServerLoad = async ({ params, url }) => {
 		const whereFilter = and(eq(examTable.patientId, patientId), eq(examTable.deleted, false));
 
 		const countTotal = await tx.select({ count: count() }).from(examTable).where(whereFilter);
-		const patientExams = await tx
-			.select()
-			.from(examTable)
-			.where(whereFilter)
-			.orderBy(desc(examTable.createdAt))
-			.limit(limit)
-			.offset(skip);
+
+		const patientExams = await tx.query.exam.findMany({
+			limit,
+			offset: skip,
+			where: whereFilter,
+			orderBy: (examTable, { desc }) => [desc(examTable.createdAt)],
+			columns: {
+				id: true,
+				createdAt: true,
+				priority: true,
+				status: true,
+				deliveredAt: true,
+				paid: true
+			},
+			with: {
+				examType: {
+					columns: { name: true }
+				}
+			}
+		});
 
 		return { count: countTotal, data: patientExams };
 	});
 
-	const patienExamsData2 = [
-		{
-			id: 'd9dee7d4-34c8-4e55-bbae-4d8e5343ed8f',
-			createdAt: '2025-05-20T05:03:08.634Z',
-			// examTypeId: '673f4311-2c99-42cf-875c-d2cb82bfb59b',
-			priority: 'normal',
-			status: 'active',
-			deliveredAt: '2025-05-20T05:03:08.634Z',
-			paid: false
-		}
-	];
-
-	console.log('countTotal: ', JSON.stringify(countTotal, null, 2));
-	console.log('patienExamsData: ', JSON.stringify(patienExamsData, null, 2));
-
-	// Create the form
+	// Create the form for deleting (if desired)
 	const deletePatientForm = await superValidate({ patientId }, zod(deletePatientSchema));
 
 	return { deletePatientForm, patienExamsData, countTotal: countTotal[0].count };
