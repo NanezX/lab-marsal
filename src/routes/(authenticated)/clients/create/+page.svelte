f5e3d572a42d7f4e7f982f304acc735a824beca4
NanezX <victor.nanej@gmail.com>
2025-05-22 00:17:16 -0400

    base page

---
+++ src/routes/(authenticated)/clients/create/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import { showToast } from '$lib/toasts.js';
+	import { goto } from '$app/navigation';
+	import { fade } from 'svelte/transition';
+
+	let { data } = $props();
+
+	const { form, errors, enhance } = superForm(data.examTypeForm, {
+		dataType: 'json',
+		delayMs: 0,
+		applyAction: true,
+		onUpdated({ form }) {
+			// Display message based on the response
+			if (form.message) {
+				showToast(form.message.text, form.message.type);
+
+				if (form.message.type == 'success') {
+					goto('/clients');
+				}
+			}
+		}
+	});
+</script>
+
+<form in:fade class="mb-4 flex w-full flex-col gap-y-8" use:enhance method="POST">
+	<!--  -->
+</form>
