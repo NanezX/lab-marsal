658c6e29faa9d47b9a9532f7a9d23751c4e40826
NanezX <victor.nanej@gmail.com>
2025-05-22 16:17:28 -0400

    feat: add icon support to LabelValue component with customizable iconClass

--- src/lib/components/LabelValue.svelte
+++ src/lib/components/LabelValue.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { ClassValue } from 'svelte/elements';
+	import { Icon, type IconSource } from '@steeze-ui/svelte-icon';
 
 	type PropType = {
 		label: string;
@@ -7,6 +8,8 @@
 		class?: ClassValue;
 		title?: string;
 		labelClass?: ClassValue;
+		icon?: IconSource;
+		iconClass?: ClassValue;
 	};
 
 	let {
@@ -14,11 +17,16 @@
 		value,
 		class: className = 'flex items-center gap-x-1',
 		title,
-		labelClass
+		labelClass,
+		icon,
+		iconClass = 'h-5 w-5'
 	}: PropType = $props();
 </script>
 
 <p {title} class={className}>
+	{#if icon}
+		<Icon src={icon} class={iconClass} />
+	{/if}
 	<span class={['font-bold', labelClass]}>{label}: </span>
 	<span>{value}</span>
 </p>
