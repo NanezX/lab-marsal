d5eb7ee36435c2cc090346fc9b6589eae3f67a0a
NanezX <victor.nanej@gmail.com>
2025-05-22 15:44:21 -0400

    feat: implement patient deletion form with validation

--- src/routes/(authenticated)/clients/[client_id]/+page.server.ts
+++ src/routes/(authenticated)/clients/[client_id]/+page.server.ts
@@ -1,1 +1,16 @@
-// TODO: Support delete. It should delete soft delete the Patient and all the related exams
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { deletePatientSchema } from '$lib/server/utils/zod';
+import type { PageServerLoad } from './$types';
+
+// TODO: Verify what roles can delete an patient (on the action) - (maybe just block the page to those user in the backend)
+
+export const load: PageServerLoad = async ({ params }) => {
+	// Create the form
+	const deletePatientForm = await superValidate(
+		{ patientId: params.client_id },
+		zod(deletePatientSchema)
+	);
+
+	return { deletePatientForm };
+};
