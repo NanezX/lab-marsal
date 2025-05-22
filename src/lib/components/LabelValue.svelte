211ea83200ba006723d43b913ef5f13a5b5e6178
NanezX <victor.nanej@gmail.com>
2025-05-22 16:13:06 -0400

    feat: add labelClass prop for customizable label styling in LabelValue component

--- src/lib/components/LabelValue.svelte
+++ src/lib/components/LabelValue.svelte
@@ -6,12 +6,13 @@
 		value: string | number;
 		class?: ClassValue;
 		title?: string;
+		labelClass?: ClassValue;
 	};
 
-	let { label, value, class: className, title }: PropType = $props();
+	let { label, value, class: className, title, labelClass }: PropType = $props();
 </script>
 
 <p {title} class={className}>
-	<span class="font-semibold">{label}: </span>
+	<span class={['font-bold', labelClass]}>{label}: </span>
 	<span>{value}</span>
 </p>
