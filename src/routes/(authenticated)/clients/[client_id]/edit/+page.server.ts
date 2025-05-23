6730dc4b65b5f6da6b8501a9b512f16b85ddaa85
NanezX <victor.nanej@gmail.com>
2025-05-22 23:59:50 -0400

    fix: implement load function to handle patient data editing and validation

--- src/routes/(authenticated)/clients/[client_id]/edit/+page.server.ts
+++ src/routes/(authenticated)/clients/[client_id]/edit/+page.server.ts
@@ -1,1 +1,23 @@
+import { editPatientSchema } from '$lib/server/utils/zod';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { cleanEditPatientData } from '$lib/shared/utils';
+import type { PageServerLoad } from './$types';
+
 // TODO: Support edit. Like firstName, lastName, documentId, birthdate, gender, email phoneNumber
+
+export const load: PageServerLoad = async ({ parent }) => {
+	// Get the data from the layout
+	const data = await parent();
+
+	// Get the examTypeData
+	const { patientData } = data;
+
+	// Clean/format the data for the schema
+	const cleaned = cleanEditPatientData(patientData);
+
+	// Create the form for deleting (if desired)
+	const editPatientForm = await superValidate(cleaned, zod(editPatientSchema));
+
+	return { editPatientForm };
+};
