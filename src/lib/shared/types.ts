3e30bd64458ad8e54801156abe1cfc26afbad1a7
NanezX <victor.nanej@gmail.com>
2025-05-22 23:59:22 -0400

    fix: add Patient type and PatientKey extraction for improved patient data handling

--- src/lib/shared/types.ts
+++ src/lib/shared/types.ts
@@ -1,7 +1,15 @@
-import type { findExamTypeById } from '$lib/server/utils/dbQueries';
+import type { patient as patientTable } from '$lib/server/db/schema';
+import type { findExamTypeById, findPatientById } from '$lib/server/utils/dbQueries';
+import type { InferSelectModel } from 'drizzle-orm';
 
 // Get the return type of `findExamTypeById`
 export type ExamTypeWithParameters = NonNullable<Awaited<ReturnType<typeof findExamTypeById>>>;
 
 // Extract the `Parameter` type from the `parameters` array
 export type Parameter = ExamTypeWithParameters['parameters'][number];
+
+export type PatientKey = keyof InferSelectModel<typeof patientTable>;
+
+export type Patient<E extends PatientKey[] = []> = NonNullable<
+	Awaited<ReturnType<typeof findPatientById<E>>>
+>;
