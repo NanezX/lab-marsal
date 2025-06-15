e67dfe761da8570e5ab8515d2b161ed1b8fcd028
NanezX <victor.nanej@gmail.com>
2025-06-15 14:01:48 -0400

    comments

--- src/lib/client/enumItems.ts
+++ src/lib/client/enumItems.ts
@@ -1,11 +1,17 @@
 import { ExamPriority, PatientGender, UserRoles, ExamStatus } from '../shared/enums';
 import { formatCapital } from '../shared/utils';
 
+/**
+ * Items for User Roles
+ */
 export const userRolesItems = Object.values(UserRoles).map((role_) => ({
 	value: role_,
 	label: formatCapital(role_)
 }));
 
+/**
+ * Item for priority
+ */
 export const priorityItems = Object.values(ExamPriority).map((priority_) => ({
 	value: priority_,
 	label: formatCapital(
@@ -19,11 +25,18 @@ const statusLabels: Record<ExamStatus, string> = {
 	[ExamStatus.Ready]: 'Listo para entregar',
 	[ExamStatus.Completed]: 'Completado'
 };
+
+/**
+ * Items for the exam status
+ */
 export const examStatusItems = Object.entries(statusLabels).map(([value, label]) => ({
 	value: value as ExamStatus,
 	label: formatCapital(label)
 }));
 
+/**
+ * Items for Gender
+ */
 export const patientGenderItems = Object.values(PatientGender).map((role_) => ({
 	value: role_,
 	label: formatCapital(role_ === PatientGender.Female ? 'Mujer' : 'Hombre')
