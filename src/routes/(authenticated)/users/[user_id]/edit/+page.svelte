82fac639d253e8c25c002d759a293982c63d6d75
NanezX <victor.nanej@gmail.com>
2025-06-16 19:56:32 -0400

    feat: refactor user edit page to improve form handling and change detection

--- src/routes/(authenticated)/users/[user_id]/edit/+page.svelte
+++ src/routes/(authenticated)/users/[user_id]/edit/+page.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import { fade } from 'svelte/transition';
+	import BackButton from '$lib/components/buttons/BackButton.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import { cleanEditUserData } from '$lib/shared/utils.js';
+	import Select from '$lib/components/Select.svelte';
+	import Button from '$lib/components/Button.svelte';
 	import CloseNavigationGuard from '$lib/components/modal/CloseNavigationGuard.svelte';
+	import { isEqual } from 'lodash-es';
 	import ConfirmModal from '$lib/components/modal/ConfirmModal.svelte';
 	import type { PageProps } from './$types';
 
+	let { data }: PageProps = $props();
+
+	let { editUserForm, userData } = data;
+
+	const {
+		form,
+		errors,
+		constraints,
+		enhance,
+		submit: submitChanges
+	} = superForm(editUserForm, {
+		dataType: 'json',
+		delayMs: 0,
+		applyAction: true,
+		onResult(event) {
+			// If forms result type is a redirect or success, we assume that the changes were saved
+			if (event.result.type === 'redirect' || event.result.type === 'success') {
+				hasChanges = false;
+			}
+		}
+	});
+
 	// Compare form vs original data
-	// let hasChanges = $derived(!isEqual($form, original));
-	let hasChanges = $state(false);
+	const original = cleanEditUserData(userData);
+
+	let hasChanges = $derived(!isEqual($form, original));
 	let showConfirmModal = $state(false);
 	let showDiscardModal = $state(false);
 </script>
@@ -15,11 +46,11 @@
 <ConfirmModal
 	bind:showModal={showConfirmModal}
 	title="Confirmar cambios"
-	secondaryText="Esto puede afectar  el acceso del usuario"
+	secondaryText="Esto puede afectar el acceso del usuario"
 	saveButtonText="Guardar cambios"
 	cancelButtonText="Cancelar"
 	onSave={() => {
-		// submitChanges();
+		submitChanges();
 		return true;
 	}}
 />
