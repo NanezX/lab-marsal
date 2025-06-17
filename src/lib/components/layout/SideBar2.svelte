89acb1da38bbbbc51e73156ed13b107da9b4cb3b
NanezX <victor.nanej@gmail.com>
2025-06-17 19:38:11 -0400

    wip

---
+++ src/lib/components/layout/SideBar2.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+	import {
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarButton,
+		SidebarBrand,
+		uiHelpers,
+		CloseButton
+	} from 'flowbite-svelte';
+	import { ChartOutline, GridSolid, MailBoxSolid, UserSolid } from 'flowbite-svelte-icons';
+	// import PlusPlaceholder from '../../utils/PlusPlaceholder.svelte';
+	import { page } from '$app/state';
+	let activeUrl = $state(page.url.pathname);
+	const spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	const imgClass = 'h-6 w-6';
+	const demoSidebarUi = uiHelpers();
+	let isDemoOpen = $state(false);
+	const closeDemoSidebar = demoSidebarUi.close;
+	import icon from '$lib/assets/icon.png';
+
+	$effect(() => {
+		isDemoOpen = demoSidebarUi.isOpen;
+		activeUrl = page.url.pathname;
+	});
+	const site = {
+		name: 'Flowbite Svelte',
+		href: '/home',
+		img: 'src/lib/assets/icon.png'
+	};
+</script>
+
+<SidebarButton onclick={demoSidebarUi.toggle} class="mb-2" />
+<div class="relative">
+	<Sidebar
+		{activeUrl}
+		backdrop={false}
+		isOpen={isDemoOpen}
+		closeSidebar={closeDemoSidebar}
+		params={{ x: -50, duration: 50 }}
+		class="z-50 h-full"
+		position="absolute"
+		activeClass="p-2"
+		nonActiveClass="p-2"
+	>
+		<CloseButton
+			onclick={closeDemoSidebar}
+			color="gray"
+			class="absolute top-3 right-1 p-2 md:hidden"
+		/>
+		<SidebarGroup>
+			<SidebarBrand {site} {imgClass} />
+			<SidebarItem label="Dashboard" href="/home">
+				{#snippet icon()}
+					<ChartOutline
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+			</SidebarItem>
+			<SidebarItem label="Kanban" {spanClass} href="/">
+				{#snippet icon()}
+					<GridSolid
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+				{#snippet subtext()}
+					<span
+						class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+						>Pro</span
+					>
+				{/snippet}
+			</SidebarItem>
+			<SidebarItem label="Inbox" {spanClass} href="/">
+				{#snippet icon()}
+					<MailBoxSolid
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+				{#snippet subtext()}
+					<span
+						class="bg-primary-200 text-primary-600 dark:bg-primary-900 dark:text-primary-200 ms-3 inline-flex h-3 w-3 items-center justify-center rounded-full p-3 text-sm font-medium"
+						>3</span
+					>
+				{/snippet}
+			</SidebarItem>
+			<SidebarItem label="Sidebar" href="/components/sidebar">
+				{#snippet icon()}
+					<UserSolid
+						class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+				{/snippet}
+			</SidebarItem>
+		</SidebarGroup>
+	</Sidebar>
+	<div class="h-96 overflow-auto px-4 md:ml-64">
+		<div class="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
+			<p>XD_1</p>
+			<p>XD_2</p>
+			<p>XD_3</p>
+			<p>XD_4</p>
+			<p>XD_5</p>
+		</div>
+	</div>
+</div>
--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -6,18 +6,97 @@
 	import { page } from '$app/state';
 
 	let { children, data } = $props();
+	///
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarButton, uiHelpers } from 'flowbite-svelte';
+	import {
+		HomeOutline,
+		MicroscopeOutline,
+		UsersGroupOutline,
+		BookOpenReaderOutline,
+		UserEditOutline,
+		AdjustmentsVerticalOutline
+	} from 'flowbite-svelte-icons';
+	let activeUrl = $state(page.url.pathname);
+	const spanClass = 'flex-1 ms-3 whitespace-nowrap';
+	const demoSidebarUi = uiHelpers();
+	let isDemoOpen = $state(false);
+	const closeDemoSidebar = demoSidebarUi.close;
+	$effect(() => {
+		isDemoOpen = demoSidebarUi.isOpen;
+		activeUrl = page.url.pathname;
+	});
 </script>
 
 <div class="h-full w-full">
 	<NavBar fullName={`${data.user.firstName} ${data.user.lastName}`} email={data.user.email} />
 	<div class="mx-auto mt-10 flex justify-evenly px-40">
-		<div class="space-y-10">
-			<SideBar />
-
-			{#if page.url.pathname == '/exam-types/create'}
-				<ScientificNotations class="h-fit w-fit bg-white" />
-			{/if}
-		</div>
+		<SidebarButton onclick={demoSidebarUi.toggle} class="mb-2" />
+		<Sidebar
+			{activeUrl}
+			backdrop={false}
+			isOpen={isDemoOpen}
+			closeSidebar={closeDemoSidebar}
+			params={{ x: -50, duration: 50 }}
+			class="top-auto ml-auto h-fit rounded-xl border border-gray-200 bg-gray-100 p-1 text-xl shadow-2xl"
+			position="static"
+			activeClass="p-2"
+			nonActiveClass="p-2"
+		>
+			<SidebarGroup>
+				<SidebarItem label="Inicio" href="/home">
+					{#snippet icon()}
+						<HomeOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+				<SidebarItem label="Exámenes" href="/exams">
+					{#snippet icon()}
+						<MicroscopeOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+				<SidebarItem label="Clientes" href="/clients">
+					{#snippet icon()}
+						<UsersGroupOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+				<SidebarItem label="Tipos exámenes" href="/exam-types">
+					{#snippet icon()}
+						<BookOpenReaderOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+				<SidebarItem label="Gestión de usuarios" href="/users">
+					{#snippet icon()}
+						<UserEditOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+				<SidebarItem label="Configuración" href="/config">
+					{#snippet icon()}
+						<AdjustmentsVerticalOutline
+							class="h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+						/>
+					{/snippet}
+				</SidebarItem>
+			</SidebarGroup>
+		</Sidebar>
+		<button
+			class="cursor-pointer"
+			onclick={() => {
+				console.log('thisss');
+				console.log('thisss2');
+				isDemoOpen = !isDemoOpen;
+			}}
+		>
+			xdaver</button
+		>
 
 		<Container class="direct-children:p-8 mx-0 mb-8 flex w-2/3 max-w-5xl rounded-xl bg-white">
 			{@render children()}
