924953775ae33b1f07885b7d68ce2c9e3fbb4d55
NanezX <victor.nanej@gmail.com>
2025-06-17 22:07:15 -0400

    feat: add IconRol component for user role icons

---
+++ src/lib/components/IconRol.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { UserRoles } from '$lib/shared/enums';
+	import {
+		ShieldOutline,
+		MicroscopeOutline,
+		SeedlingOutline,
+		FolderOutline
+	} from 'flowbite-svelte-icons';
+	import type { ClassValue } from 'svelte/elements';
+
+	// Prop type
+	type PropType = {
+		role: UserRoles;
+		class?: ClassValue;
+	};
+
+	let { role, class: className = 'text-primary-blue h-5 w-5' }: PropType = $props();
+
+	const IconRoles: Record<string, typeof ShieldOutline> = {
+		[UserRoles.Admin]: ShieldOutline,
+		[UserRoles.Auxiliar]: SeedlingOutline,
+		[UserRoles.Bioanalista]: MicroscopeOutline,
+		[UserRoles.Secretaria]: FolderOutline
+	};
+
+	const C = IconRoles[role];
+</script>
+
+<C class={className} />
