f240484c4097f45e040f38a404ef7aec6920eec2
NanezX <victor.nanej@gmail.com>
2025-06-19 01:39:51 -0400

    feat: add user profile edit functionality with confirmation modal

--- src/routes/(authenticated)/account/edit/+page.svelte
+++ src/routes/(authenticated)/account/edit/+page.svelte
@@ -1,1 +1,39 @@
-Editar cuenta
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/Button.svelte';
+	import BackButton from '$lib/components/buttons/BackButton.svelte';
+	import Checkbox from '$lib/components/Checkbox.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import ConfirmModal from '$lib/components/modal/ConfirmModal.svelte';
+	import { LockOpen2, Lock } from '@steeze-ui/tabler-icons';
+	import { fade } from 'svelte/transition';
+	import { superForm } from 'sveltekit-superforms';
+
+	let { data } = $props();
+
+	const {
+		form,
+		errors,
+		constraints,
+		enhance,
+		submit: submitChanges
+	} = superForm(data.editProfileForm, {
+		dataType: 'json',
+		delayMs: 0,
+		applyAction: true
+	});
+
+	let showConfirmModal = $state(false);
+</script>
+
+<ConfirmModal
+	bind:showModal={showConfirmModal}
+	title="Confirmar cambio"
+	secondaryText="Esta acción cambiará la información reflejada en tu perfil."
+	saveButtonText="Confirmar"
+	cancelButtonText="Cancelar"
+	onSave={() => {
+		submitChanges();
+		return true;
+	}}
+/>
