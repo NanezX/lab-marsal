5e3e6893e48c46c4cf3c76561795849269bd4f8f
NanezX <victor.nanej@gmail.com>
2025-06-19 01:51:54 -0400

    feat: enhance user profile form with additional fields and improved binding logic

--- src/routes/(authenticated)/account/edit/+page.svelte
+++ src/routes/(authenticated)/account/edit/+page.svelte
@@ -44,4 +44,72 @@
 
 		<p class="mx-auto text-center text-3xl">Editar perfil</p>
 	</div>
+
+	<div>
+		<div class="space-y-5">
+			<p class="text-2xl">Datos personales</p>
+
+			<div class="space-y-4">
+				<div class="flex gap-x-8">
+					<Input
+						bind:value={$form.firstName}
+						name="firstName"
+						label="Nombre"
+						placeholder="Nombre del paciente"
+						wrapperClass="w-1/2"
+						error={$errors.firstName}
+					/>
+
+					<Input
+						bind:value={$form.lastName}
+						name="lastName"
+						label="Apellido"
+						placeholder="Apellido del paciente"
+						wrapperClass="w-1/2"
+						error={$errors.lastName}
+					/>
+				</div>
+
+				<div class="flex gap-x-8">
+					<Input
+						bind:value={
+							() => ($form.documentId === 0 ? '' : $form.documentId),
+							(v) => ($form.documentId = v === '' ? 0 : v)
+						}
+						name="documentId"
+						label="Cédula de Identidad"
+						placeholder="Cédula"
+						wrapperClass="w-1/3"
+						type="number"
+						autoComplete={false}
+						error={$errors.documentId}
+					/>
+
+					<Input
+						bind:value={$form.birthdate}
+						name="birthdate"
+						label="Cumpleaños"
+						type="date"
+						required
+						placeholder="Cumpleaños"
+						wrapperClass="w-1/3"
+						{...$constraints.birthdate}
+					/>
+				</div>
+
+				<div class="mt-8 flex gap-x-8">
+					<Input
+						bind:value={() => $form.email ?? '', (v) => ($form.email = v)}
+						type="email"
+						name="email"
+						label="Correo electrónico (opcional)"
+						placeholder="Correo electrónico"
+						autoComplete={false}
+						wrapperClass="w-1/2"
+						error={$errors.email}
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
 </form>
