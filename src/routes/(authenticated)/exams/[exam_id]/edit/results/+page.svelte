b3543d3aed84c344dd1bd22d22495f824b63b4b2
NanezX <victor.nanej@gmail.com>
2025-06-19 20:50:34 -0400

    feat: implement edit exam results form with enhanced structure and functionality

--- src/routes/(authenticated)/exams/[exam_id]/edit/results/+page.svelte
+++ src/routes/(authenticated)/exams/[exam_id]/edit/results/+page.svelte
@@ -1,1 +1,54 @@
-<p>Edit results</p>
+<script>
+	import { superForm } from 'sveltekit-superforms';
+	import { fade } from 'svelte/transition';
+	import BackButton from '$lib/components/buttons/BackButton.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import { cleanEditExamDetails } from '$lib/shared/utils.js';
+	import Select from '$lib/components/Select.svelte';
+	import { PatientGender } from '$lib/shared/enums.js';
+	import Button from '$lib/components/Button.svelte';
+	import CloseNavigationGuard from '$lib/components/modal/CloseNavigationGuard.svelte';
+	import { isEqual } from 'lodash-es';
+	import ConfirmModal from '$lib/components/modal/ConfirmModal.svelte';
+	import {
+		Id,
+		GenderMale,
+		GenderFemale,
+		User,
+		Cash,
+		ListTree,
+		ClockPlus,
+		ClockEdit
+	} from '@steeze-ui/tabler-icons';
+	import LabelValue from '$lib/components/LabelValue.svelte';
+	import { examStatusItems, priorityItems } from '$lib/client/enumItems.js';
+
+	let { data } = $props();
+
+	let { editExamResultsForm, examData } = data;
+
+	const {
+		form,
+		errors,
+		enhance,
+		submit: submitChanges
+	} = superForm(editExamResultsForm, {
+		dataType: 'json',
+		delayMs: 0,
+		applyAction: true,
+		onResult(event) {
+			// If forms result type is a redirect or success, we assume that the changes were saved
+			if (event.result.type === 'redirect' || event.result.type === 'success') {
+				// hasChanges = false;
+			}
+		}
+	});
+</script>
+
+<form in:fade class="mb-4 flex w-full flex-col gap-y-8" use:enhance method="POST">
+	<div class="relative flex justify-center">
+		<BackButton href="/exams/{examData.id}" size="40" />
+
+		<p class="mx-auto text-center text-3xl">Editar resultados</p>
+	</div>
+</form>
