21f578858e24f9b74905f79febbf97f15cd33afa
NanezX <victor.nanej@gmail.com>
2025-07-06 01:27:51 -0400

    remove console

--- src/routes/(authenticated)/report/+page.server.ts
+++ src/routes/(authenticated)/report/+page.server.ts
@@ -4,6 +4,5 @@ import { getChartData } from '$lib/server/chartData';
 export async function load() {
 	const chartData = await getChartData();
 
-    console.log('chartData: ', chartData)
 	return { chartData };
 }
